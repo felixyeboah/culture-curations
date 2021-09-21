07df970b156fc9fe96652e95eb947a1e80f151a6
Felix Nana Yaw Yeboah - Jefferson <87368189+felixyeboah@users.noreply.github.com>
2021-09-21 02:59:28 +0000

    almost done

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,12 +1,12 @@
-import useAuth from "context/userContext";
+// import useAuth from "context/userContext";
 import AuthLayout from "container/AuthLayout";
 import React from "react";
 
 const Dashboard = () => {
-  const { user, active } = useAuth();
-
-  console.log("user", user);
-  console.log("active", active);
+  // const { user, active } = useAuth();
+  //
+  // console.log("user", user);
+  // console.log("active", active);
 
   return (
     <AuthLayout title="Dashboard">
